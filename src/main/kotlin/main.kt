23b0370d1fa229db0aa71edbd344141e403a7f7f
Nikiforov <rx992.ya.ru>
2025-04-13 15:09:37 +0700

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main(){
+    println("Hello Kotlin")
+    val scanner = Scanner(System.`in`)
+    println("enter number: ")
+    val number = scanner.nextInt()
+    val sum = 100 + number
+    println(sum)
+
+}
+
